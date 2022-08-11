5727c1d0fc199db09b16e13058e96d90cbfe28dc
AndreyMachula <amcz2020@gmail.com>
2022-08-11 15:59:43 +0200

    adds constant => theme

---
+++ src/constants/theme.js
@@ -0,0 +1,48 @@
+const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    green: '#4caf50',
+    red: '#f44336',
+    blue: '#2196f3',
+    primary: '#07c',
+    secondary: '#05a',
+    lightColor: '#E5E4E2',
+    primaryText: '#212121',
+    secondaryText: '#757575',
+    muted: '#f6f6f6',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256], // for margin and padding
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSize: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '24px',
+    xl: '32px',
+    xxl: '64px',
+  },
+  fontWeight: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeight: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '5px',
+    round: '50%',
+  },
+};
+
+export default theme;
